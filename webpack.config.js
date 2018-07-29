270950b590a08b1f2871f98412bc62206c1ed384
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-07-29 11:54:14 -0500

    Entorno de producción: Elimina directorio para generación de archivos de distribución
    Aclaración:
    Al eliminar el directorio de archivos de distribución antes de volver
    a generar nuevamente estos archivos, pretende eliminar todos aquellos
    archivos que se generan transpilación tras transpilación y que no se
    usarán debido que al usar hashes, cambian de nombre y no resultan ser
    archivos definitivos, por lo cual si no se limpia este tras la trasnpilación
    repetida este directorio crecerá y no vamos a querer subir archivos
    innecesarios
    
    Hemos agregado adicionalmente la posibilidad que el archivo de salida
    sea generado con su nombre dínámico concatenado a un hash dinámico
    también
    
    Para que esta funcionalidad pueda ser agregada como plugin a la
    configuración de Webpack debemos instalar el paquete 'clean-webpack-plugin'
    
       npm i -D clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,68 @@
 const path = require( 'path' );                                     /* Paquete nativo en Webpack */
 const ExtratTextPlugin = require( 'extract-text-webpack-plugin' );  /* Plugin para extraer archivos de Texto (CSS) Requiere instalación */
+const CleanWebpackPlugin = require( 'clean-webpack-plugin' );       /* Plugin para Limpiar todo lo que esté en el directorio 'dist' Requiere instalación */
 
-const plugins = [
-    new ExtratTextPlugin( 'css/[name].css' )        /* Todos los archivos extraidos iran a esta ruta con nombres dinámicos*/
-];
+/* Cambiamos la exportación de un Objeto JSON a un Arroy Function para que pueda reconocer la variable de entorno 'env' */
+module .exports = ( env ) => {
+    /* Definición de Plugins */
+    const plugins = [
+        new ExtratTextPlugin( 'css/[name].css' )        /* Todos los archivos extraidos iran a esta ruta con nombres dinámicos*/
+    ];
 
-/* Valida las variables de entorno de NODE definidas en el Script lanzado */
-if( env .NODE_ENV === 'production' ) {
-    plugins .push(
+    /* Valida las variables de entorno de NODE definidas en el Script lanzado */
+    if( env .NODE_ENV === 'production' ) {
+        plugins .push(
+            new CleanWebpackPlugin( [ 'dist' ], { root: __dirname } )
+        );
+    }
 
-    );
-}
-
-module .exports = {
-    entry: {
-        invie: path .resolve( __dirname, 'src/index.js' )
-    },
-    output: {
-        path: path .resolve( __dirname, 'dist' ),                /* Path de archivos de salida */
-        filename: 'js/[name].js',                                /* Directorio de destino y nombre dinámico de archivos */
-        publicPath: path .resolve( __dirname, 'dist' ) + '/',    /* Nombre del path público donde encontrará los archivos generados */
-        chunkFilename: 'js/[id].[chunkhash].js'                  /* Como se van a llamar los fragmentos de código que estamos generando (en nuestro caso los módulos: 0.js )*/
-    },
-    devServer: {
-        port: 9000                                  /* Por defecto el puerto es 8080 */
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,                                /* Soporte para extenciones .js y jsx (ReactJS) */
-                exclude: /(node_modules)/,                          /* Directorios excluidos de transpilación */
-                use: {
-                    loader: 'babel-loader',
+    return {
+        entry: {
+            invie: path .resolve( __dirname, 'src/index.js' )
+        },
+        output: {
+            path: path .resolve( __dirname, 'dist' ),                /* Path de archivos de salida */
+            filename: 'js/[name].[hash].js',                         /* Directorio de destino y nombre dinámico de archivos */
+            publicPath: path .resolve( __dirname, 'dist' ) + '/',    /* Nombre del path público donde encontrará los archivos generados */
+            chunkFilename: 'js/[id].[chunkhash].js'                  /* Como se van a llamar los fragmentos de código que estamos generando (en nuestro caso los módulos: 0.js )*/
+        },
+        devServer: {
+            port: 9000                                  /* Por defecto el puerto es 8080 */
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,                                /* Soporte para extenciones .js y jsx (ReactJS) */
+                    exclude: /(node_modules)/,                          /* Directorios excluidos de transpilación */
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [ 'es2015', 'react', 'stage-2' ]   /* Habilita: ES6, React, Spread Operator usando Babel */
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    /* Implementa el Plugin instalado (extract-text-webpack-plugin) */
+                    use: ExtratTextPlugin .extract({
+                        loader: 'css-loader',                       /* Interpresa el CSS */
+                        options: {
+                            minimize: true,                         /* Minificación */
+                            modules: true                           /* Permite la interpretación de CSS dentro de otros CSS */
+                        }
+                    })
+                },
+                {
+                    test: /\.(jpg|png|gif|svg)$/,                       /* Soporte para extenciones para imagenes */
+                    loader: 'url-loader',
                     options: {
-                        presets: [ 'es2015', 'react', 'stage-2' ]   /* Habilita: ES6, React, Spread Operator usando Babel */
+                        limit: 1000000,                     /* Tamaño máximo */
+                        fallBack: 'file-loader',            /* Por defecto usa 'publicPath' */
+                        name: 'images/[name].[hash].[ext]'  /* Directorio de destino y nombre dinámico, hash dinámico y extensión dinámica de archivos */
                     }
                 }
-            },
-            {
-                test: /\.css$/,
-                /* Implementa el Plugin instalado (extract-text-webpack-plugin) */
-                use: ExtratTextPlugin .extract({
-                    loader: 'css-loader',                       /* Interpresa el CSS */
-                    options: {
-                        minimize: true,                         /* Minificación */
-                        modules: true                           /* Permite la interpretación de CSS dentro de otros CSS */
-                    }
-                })
-            },
-            {
-                test: /\.(jpg|png|gif|svg)$/,                       /* Soporte para extenciones para imagenes */
-                loader: 'url-loader',
-                options: {
-                    limit: 1000000,                     /* Tamaño máximo */
-                    fallBack: 'file-loader',            /* Por defecto usa 'publicPath' */
-                    name: 'images/[name].[hash].[ext]'  /* Directorio de destino y nombre dinámico, hash dinámico y extensión dinámica de archivos */
-                }
-            }
-        ]
-    },
-    plugins
+            ]
+        },
+        plugins
+    }
 }
